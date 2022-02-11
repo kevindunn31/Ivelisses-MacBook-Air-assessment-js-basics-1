9aaf73561542c60b55e7642daf96acbb9187ec9e
Kevin Dunn <kevindunn31@gmail.com>
2022-02-11 13:14:52 -0600

    Assessment actually completed

---
+++ password.js
@@ -0,0 +1,14 @@
+//Creating a password 
+
+var password = prompt ("Welcome the user to the password validator tool")
+
+
+numsArr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 0]
+lettersArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o' , 'p', 'q', 'r', 's', 't', 'v', 'u', 'w', 'x', 'y', 'z']
+
+for (let i = 0; i <= 10; i++){
+    if (nummArr[i] >= 10 && lettersArr[i] >= 10){
+        console.log("Acceptable Password")
+    }
+}
+
